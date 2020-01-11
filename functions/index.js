0af99fd49af74424482332857b511a9f85b26644
Joffrey Devautour <joffrey.devautour@epitech.eu>
2020-01-11 18:40:06 +0100

    [ADD] Cloud function example

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,20 @@
-const functions = require('firebase-functions');
+/** EXPORT ALL FUNCTIONS
+ *
+ *   Loads all `.f.js` files
+ *   Exports a cloud function matching the file name
+ *   Author: David King
+ *   Edited: Tarik Huber
+ *   Based on this thread:
+ *     https://github.com/firebase/functions-samples/issues/170
+ */
+const glob = require("glob");
+const camelCase = require("camelcase");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const files = glob.sync('./**/*.f.js', { cwd: __dirname, ignore: './node_modules/**' });
+for (let f = 0, fl = files.length; f < fl; f++) {
+  const file = files[f];
+  const functionName = camelCase(file.slice(0, -5).split('/').join('_'));
+  if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === functionName) {
+    exports[functionName] = require(file);
+  }
+}
